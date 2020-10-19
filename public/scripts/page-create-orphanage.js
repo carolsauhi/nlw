05e24dc3276fcdcf29bad053d04e5bf345a160e6
Carol Sauhi <carol_sauhi@hotmail.com>
2020-10-18 22:30:00 -0300

    Commit

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -1,95 +1,82 @@
 //create map
-const map = L.map('mapid').setView([-22.929623,-47.0614431], 15);
+const map = L.map('mapid').setView([-27.2109325,-49.6448719], 15);
 
-//create and add tileLayer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
+.addTo(map);
 
-//create icon
+
 const icon = L.icon({
-    iconUrl: './public/images/map-marker.svg',
-    iconSize: [58,68],
-    iconAnchor: [29, 68],
+    iconUrl: "/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
 })
 
 let marker;
 
-//create and add marker
 map.on('click', (event) => {
-    const lat = event.latlng.lat;
-    const lng = event.latlng.lng;
+    const lat = event.latlng.lat
+    const lng = event.latlng.lng
 
-    document.querySelector('[name=lat]').value = lat;
-    document.querySelector('[name=lng]').value = lng;
+    document.querySelector('[name=lat]').value = lat
+    document.querySelector('[name=lng]').value = lng
 
-    //remover icon
-    marker &&  map.removeLayer(marker)
+    marker && map.removeLayer(marker)
 
-    //add icon layer
-    marker = L.marker([lat,lng], { icon })
+    marker = L.marker([lat, lng], { icon })
     .addTo(map)
-
 })
 
-//adicionar o campo de fotos
+
 function addPhotoField() {
+    const container = document.querySelector('#images');
 
-    //pegar o container de fotos #images
-    const container = document.querySelector('#images')
-
-    //pegar o container para duplicar  .new-image
     const fieldsContainer = document.querySelectorAll('.new-upload')
 
-    //realizar o clone da ultima imagem adicionada.
-    const newFieldContainer = fieldsContainer[fieldsContainer.length -1 ].cloneNode(true)
-    
-    //Verificar se o campo esta vazio, se sim, não adicionar ao container de imagens
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+
     const input = newFieldContainer.children[0]
 
-    if(input.value == ""){
+    if(input.value == "") {
         return
     }
 
-    //Limpar o campo antes de adicionar ao container de imagens
-   input.value = ""
+    input.value = ""
 
-    //adicionar o clone ao container de #images
     container.appendChild(newFieldContainer)
-    
 }
 
 function deleteField(event) {
     const span = event.currentTarget
 
     const fieldsContainer = document.querySelectorAll('.new-upload')
-    
-    if (fieldsContainer.length < 2){
-        //limpar o valor do campo
+
+    if(fieldsContainer.length <= 1) {
         span.parentNode.children[0].value = ""
-        return
+        return 
     }
 
-    //deletar o campo
     span.parentNode.remove()
-
 }
 
-//troca do sim e não
-function toggleSelect() {
-    //retirar a class .active(dos botões)
+function toggleSelect(event) {
     document.querySelectorAll('.button-select button')
-    .forEach(button => button.classList.remove('active'))
+    .forEach((button) => button.classList.remove('active'))
 
-    //colocar a class .active nesse botão clicado
+
     const button = event.currentTarget
     button.classList.add('active')
 
-    //atualizar o meu input com o valor selecionados
     const input = document.querySelector('[name="open_on_weekends"]')
-    
     input.value = button.dataset.value
-
-
-
-
-
 }
+
+// function validate(event) {
+
+//     //VALIDAR SE ESTA TUDO PREENCHIDO
+//     const needsLatAndLng = true;
+//     if (needsLatAndLng) {
+//         event.preventDefault()
+//         alert('Selecione um ponto no mapa')
+//     }
+    
+// }
--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,50 +1,49 @@
 const options = {
-    dragging : false,
+    dragging: false,
     touchZoom: false,
     doubleClickZoom: false,
     scrollWheelZoom: false,
-    zoomControl: false
+    zoomControl: false,
 }
 
-//create map
-const map = L.map('mapid', options).setView([-22.929623,-47.0614431], 15);
+//get values from html
+const lat = document.querySelector('[data-lat]').dataset.lat
+const lng = document.querySelector('[data-lng]').dataset.lng
 
-//create and add tileLayer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',).addTo(map);
+const map = L.map('mapid', options).setView([lat,lng], 15);
 
-//create icon
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
+.addTo(map);
+
 const icon = L.icon({
-    iconUrl: './public/images/map-marker.svg',
-    iconSize: [58,68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
+    iconUrl: "/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
+    popAnchor: [170, 2]
 })
 
-//create and add marker
+//create and marker
 L
-.marker([-22.929623,-47.0614431], {icon})
+.marker([lat,lng], { icon })
 .addTo(map)
 
+
 /* image gallery */
+
 function selectImage(event) {
     const button = event.currentTarget
-    
-    // remover todas as classes .active
+
     const buttons = document.querySelectorAll(".images button")
     buttons.forEach(removeActiveClass)
 
     function removeActiveClass(button) {
-        button.classList.remove("active")
+        button.classList.remove('active');
     }
 
-    // selecionar a imagem clicada
     const image = button.children[0]
-    const imageContainer = document.querySelector(".orphanage-details > img")
+    const imageContainer = document.querySelector('.orphanage-details > img')
 
-    // atualixar o container de imagem
     imageContainer.src = image.src
 
-    // adicionar a classe .active para este botão 
     button.classList.add('active')
-
-}
+} 
